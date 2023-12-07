da66af84264f589ee0b6f077454a0989dedbf846
Chao Li <cli49003@usc.edu>
2023-12-06 16:56:32 -0800

    Drag and Drop Implemented

--- script.js
+++ script.js
@@ -37,7 +37,10 @@ function addSporadicTask(event) {
 }
 
 function addTaskBlock(list) {
+  // Generate a unique ID for each task
+  const uniqueId = `task-${Date.now()}`;
   const taskBlock = createTaskBlock(generateRandomTaskName());
+  taskBlock.id = uniqueId; // Set the unique ID
   list.appendChild(taskBlock);
 
   resizeList(list);
@@ -46,6 +49,12 @@ function addTaskBlock(list) {
 function createTaskBlock(text) {
   const taskBlock = document.createElement("div");
   taskBlock.classList.add("task-block");
+  taskBlock.draggable = true; // Make the task block draggable
+
+  taskBlock.style.backgroundColor = getRandomColor();
+
+  taskBlock.addEventListener("dragstart", dragStart); // Add dragstart event listener
+  taskBlock.addEventListener("dragend", dragEnd);
 
   // Create a span to hold the task text separately from the buttons.
   const taskText = document.createElement("span");
@@ -70,6 +79,66 @@ function createTaskBlock(text) {
   return taskBlock;
 }
 
+function getRandomColor() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  const alpha = 0.5; // 50% Transparency
+  return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+}
+
+function dragStart(event) {
+  event.dataTransfer.setData("text/plain", event.target.id);
+  event.dataTransfer.effectAllowed = "move";
+}
+
+function dragOver(event) {
+  event.preventDefault();
+  event.target.classList.add("highlight"); // Highlight the list
+}
+
+function dragEnd(event) {
+  // Remove highlight from all lists
+  document
+    .querySelectorAll(".list")
+    .forEach((list) => list.classList.remove("highlight"));
+}
+
+function drop(event) {
+  event.preventDefault();
+  const data = event.dataTransfer.getData("text");
+  const draggedElement = document.getElementById(data);
+
+  // Check if the drop target is a task block
+  let dropTarget = event.target;
+  while (!dropTarget.classList.contains("list") && dropTarget.parentNode) {
+    if (dropTarget.classList.contains("task-block")) {
+      // If dropped on a task block, insert before this task block
+      dropTarget.parentNode.insertBefore(draggedElement, dropTarget);
+      resizeList(dropTarget.parentNode);
+      return; // Exit the function
+    }
+    dropTarget = dropTarget.parentNode;
+  }
+
+  // If dropped on the list (not on a task block), append to the end of the list
+  if (dropTarget.classList.contains("list")) {
+    dropTarget.appendChild(draggedElement);
+    resizeList(dropTarget);
+  }
+
+  // Remove highlight from all lists
+  document
+    .querySelectorAll(".list")
+    .forEach((list) => list.classList.remove("highlight"));
+}
+
+// Add dragover and drop event listeners to the lists
+chronologicalList.addEventListener("dragover", dragOver);
+chronologicalList.addEventListener("drop", drop);
+sporadicList.addEventListener("dragover", dragOver);
+sporadicList.addEventListener("drop", drop);
+
 function editTaskBlock(taskBlock) {
   const newText = prompt(
     "Enter new task name:",
@@ -87,10 +156,12 @@ function deleteTaskBlock(taskBlock) {
 }
 
 function resizeList(list) {
-  // Instead of using scrollHeight, we calculate the total height of all children.
   let totalHeight = 0;
   Array.from(list.children).forEach((child) => {
-    totalHeight += child.offsetHeight;
+    const style = window.getComputedStyle(child);
+    const marginTop = parseInt(style.marginTop, 10);
+    const marginBottom = parseInt(style.marginBottom, 10);
+    totalHeight += child.offsetHeight + marginTop + marginBottom;
   });
   list.style.height = `${Math.max(200, totalHeight)}px`;
 }
